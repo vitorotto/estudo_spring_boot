6eb09bf52c21dd0ed3d70ccfe2ae6fcc9bf6c061
vitorotto <vitorotto05@gmail.com>
2026-06-07 18:03:00 -0300

    Teste

--- demo/src/main/java/com/vitorotto/demo/DemoApplication.java
+++ demo/src/main/java/com/vitorotto/demo/DemoApplication.java
@@ -1,7 +1,13 @@
 package com.vitorotto.demo;
 
+import org.springframework.context.ApplicationContext;
+
+import java.util.Arrays;
+
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class DemoApplication {
@@ -10,4 +16,16 @@ public class DemoApplication {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
+	@Bean
+	public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+		return args -> {
+			System.out.println("Let's inspect the beans provided by Spring Boot:");
+
+			String[] beanNames = ctx.getBeanDefinitionNames();
+			Arrays.sort(beanNames);
+			for (String beanName : beanNames) {
+				System.out.println(beanName);
+			}
+		};
+	}
 }
